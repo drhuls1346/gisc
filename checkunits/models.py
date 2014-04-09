a38dc8ee8a76eb0c8c2de35eac9fdf49071d0979
drhuls1346 <drhuls1346@ung.edu>
2014-04-09 11:47:12 -0400

    Created Unit model.
    Created unit model with the name, utype, and status fields,__unicode__ method and registered the model in the admin.

--- checkunits/models.py
+++ checkunits/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Unit(models.Model):
+    name = models.CharField(max_length=30)
+    utype = models.CharField(max_length=30)
+    status = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return "{} - {} - Out: {}".format(self.name,
+                                          self.utype,
+                                          self.status)
